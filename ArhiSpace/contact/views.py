7ec49a1c952ffe724f00e212ffca6bc0fbfd7e86
mara-1104 <marag273@gmail.com>
2025-06-30 23:32:53 +0300

    contact

--- ArhiSpace/contact/views.py
+++ ArhiSpace/contact/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
+
+
 def contact_view(request):
-	context = {}
-	return render(request, 'contact/index.html', context)
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        full_message = f"Mesaj de la {name} ({email}):\n\n{message}"
+
+        send_mail(
+            subject="Formular Contact - Arhispace",
+            message=full_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.DEFAULT_FROM_EMAIL],
+        )
+
+        return render(request, 'contact/index.html', {'success': True})
+
+    return render(request, 'contact/index.html')
