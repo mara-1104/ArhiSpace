8fa95296c3f919c0c063fba87639faf8060bf9bc
mara <marag273@gmail.com>
2025-08-06 14:35:45 +0300

    pauza - team

--- ArhiSpace/team/views.py
+++ ArhiSpace/team/views.py
@@ -3,5 +3,49 @@ from django.shortcuts import render
 # Create your views here.
 
 def team_view(request):
-	context = {}
+	context = {'team_members' : team_members}
 	return render(request, 'team/index.html', context)
+
+team_members = [
+	{
+		'name' : 'Cosmin Florin Brezeanu',
+		'role' : 'Director General',
+		'photo' : '3images/male.png'
+	},
+
+	{
+		'name' : 'Andrei Ștefan Gagiu',
+		'role' : 'Director Marketing',
+		'photo' : '3images/male.png'
+	},
+
+	{
+		'name' : 'Vlad Cristian Mogîlă',
+		'role' : 'Director Tehnic',
+		'photo' : '3images/male.png'
+	},
+
+	{
+		'name' : 'Valentina Maria Pătrașcu',
+		'role' : 'Director Vânzări',
+		'photo' : '3images/female.png'
+	},
+
+	{
+		'name' : 'Tudor Brătucu',
+		'role' : 'Contabil Șef',
+		'photo' : '3images/male.png'
+	},
+
+	{
+		'name' : 'Sebastian Ștefan Sîrboiu',
+		'role' : 'Administrator',
+		'photo' : '3images/male.png'
+	},
+
+	{
+		'name' : 'Sergiu Alexis Adamov',
+		'role' : 'Contabil',
+		'photo' : '3images/male.png'
+	}
+]
