a63e9ea16fa8b17397dad46aa23f2f263a560908
mara <marag273@gmail.com>
2025-08-06 20:03:58 +0300

    team

--- ArhiSpace/team/views.py
+++ ArhiSpace/team/views.py
@@ -10,42 +10,42 @@ team_members = [
 	{
 		'name' : 'Cosmin Florin Brezeanu',
 		'role' : 'Director General',
-		'photo' : '3images/male.png'
+		'photo' : 'images/male.png'
 	},
 
 	{
 		'name' : 'Andrei Ștefan Gagiu',
 		'role' : 'Director Marketing',
-		'photo' : '3images/male.png'
+		'photo' : 'images/male.png'
 	},
 
 	{
 		'name' : 'Vlad Cristian Mogîlă',
 		'role' : 'Director Tehnic',
-		'photo' : '3images/male.png'
+		'photo' : 'images/male.png'
 	},
 
 	{
 		'name' : 'Valentina Maria Pătrașcu',
 		'role' : 'Director Vânzări',
-		'photo' : '3images/female.png'
+		'photo' : 'images/female.png'
 	},
 
 	{
 		'name' : 'Tudor Brătucu',
 		'role' : 'Contabil Șef',
-		'photo' : '3images/male.png'
+		'photo' : 'images/male.png'
 	},
 
 	{
 		'name' : 'Sebastian Ștefan Sîrboiu',
 		'role' : 'Administrator',
-		'photo' : '3images/male.png'
+		'photo' : 'images/male.png'
 	},
 
 	{
 		'name' : 'Sergiu Alexis Adamov',
 		'role' : 'Contabil',
-		'photo' : '3images/male.png'
+		'photo' : 'images/male.png'
 	}
 ]
